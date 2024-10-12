f6ae34e66e56ad23e31698de970fae6266edac5a
bench <work2882@outlook.com>
2024-10-11 18:46:39 -0700

    Pricing block, gallery, Our Classes screen created

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,7 +1,15 @@
+import Layout from '@components/layout/Layout'
+
 import styles from './Home.module.scss'
 
 function Home() {
-	return <div className={styles.wrapper}></div>
+	return (
+		<Layout>
+			<div className={styles.wrapper}>
+				<></>
+			</div>
+		</Layout>
+	)
 }
 
 export default Home
