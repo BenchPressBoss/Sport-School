1dddc4a069e88946f58dcca59c6cec4184e64fb0
Maxim Sychev <work2882@outlook.com>
2024-10-15 18:59:00 -0700

    C

--- src/components/screens/home/schedule/schedule-card/ScheduleCard.tsx
+++ src/components/screens/home/schedule/schedule-card/ScheduleCard.tsx
@@ -1,5 +1,25 @@
-function ScheduleCard() {
-	return <div></div>
+import styles from './ScheduleCard.module.scss'
+
+type scheduleCardTypes = {
+	time: string
+	category: string
+	during: string
+	name: string
+}
+
+function ScheduleCard({ time, category, during, name }: scheduleCardTypes) {
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.timeCategory}>
+				<p>{time}</p>
+				<p>{category}</p>
+			</div>
+			<div className={styles.duringTrainerName}>
+				<p>{during}</p>
+				<p>{name}</p>
+			</div>
+		</div>
+	)
 }
 
 export default ScheduleCard
