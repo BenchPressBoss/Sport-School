2b7409cef6c822f5da5f1f38b96dc587667e3dfc
Maxim Sychev <work2882@outlook.com>
2024-10-15 19:02:16 -0700

    First version schedule

--- src/components/screens/home/schedule/Schedule.tsx
+++ src/components/screens/home/schedule/Schedule.tsx
@@ -1,5 +1,41 @@
-function Schedule() {
-	return <></>
+import { useState } from 'react'
+
+import styles from './Schedule.module.scss'
+import ScheduleCard from './schedule-card/ScheduleCard'
+import { ScheduleData } from './schedule-card/scheduleCard.data'
+
+type scheduleTypes = {
+	weekDay: string
+	src: string
+	alt: string
+}
+function Schedule({ weekDay, src, alt }: scheduleTypes) {
+	const [isOpen, setIsOpen] = useState(false)
+	const daySchedule = ScheduleData[weekDay]
+
+	const toggleSchedule = () => setIsOpen(!isOpen)
+	return (
+		<>
+			<div className={styles.info} onClick={toggleSchedule}>
+				<p>{weekDay}</p>
+				<img src={src} alt={alt} />
+			</div>
+			<div className={styles.schedule}>
+				<div
+					className={`${styles.dayScheduleWrapper} ${isOpen ? styles.open : 'styles.closed'}`}
+				>
+					{daySchedule.map(day => (
+						<ScheduleCard
+							time={day.time}
+							category={day.category}
+							during={day.during}
+							name={day.name}
+						/>
+					))}
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Schedule
